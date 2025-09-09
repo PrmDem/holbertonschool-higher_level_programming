77b146b3c913cdfb34ce50fdc2b009bfa469c8e4
Priam <priamdmy@gmail.com>
2025-09-09 15:32:14 +0200

    [fix] missing requirement: pycode ok, chmod ok

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    # prints integers stored in a list
-    # takes 'my_list' as an argument
-    # doesn't return anything
     for item in my_list:
-        print("{}".format(item))
+        print("{:d}".format(item))
