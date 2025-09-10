b437f7baad3225c64058bbfaf6b3ed3666325ba6
Priam <priamdmy@gmail.com>
2025-09-10 16:06:41 +0200

    [fix] added back condition checks from test file

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,22 +3,24 @@ def roman_to_int(roman_string):
     correspondances = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                        'C': 100, 'D': 500, 'M': 1000}
     numList = []
-    res = 0
-    init = 0
+    res = init = 0
     next = 1
-    for letter in roman_string:
-        for key, value in correspondances.items():
-            if letter == key:
-                numList.append(value)
-    if len(numList) == 1:
-        res = numList[0]
+    if roman_string.isalpha() and roman_string is not None:
+        for letter in roman_string:
+            for key, value in correspondances.items():
+                if letter == key:
+                    numList.append(value)
+        if len(numList) == 1:
+            res = numList[0]
+        else:
+            while init < (len(numList) - 1) and next <= (len(numList) - 1):
+                if numList[init] < numList[next]:
+                    res -= numList[init]
+                elif numList[init] >= numList[next]:
+                    res += numList[init]
+                init += 1
+                next += 1
+            res += numList[init]
+        return res
     else:
-        while init < (len(numList) - 1) and next <= (len(numList) - 1):
-            if numList[init] < numList[next]:
-                res -= numList[init]
-            elif numList[init] >= numList[next]:
-                res += numList[init]
-            init += 1
-            next += 1
-        res += numList[init]
-    return res
+        return 0
