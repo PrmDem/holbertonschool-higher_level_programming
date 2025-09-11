d08b99477e28bb1af7ade3dae3d0a64c2602217c
Priam <priamdmy@gmail.com>
2025-09-11 18:22:50 +0200

    [fix] except Type&Value errors only

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,13 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(0, x):
         try:
-            if not isinstance(my_list[i], int):
-                continue
-            else:
-                print("{}" .format(my_list[i]), end='')
-                count += 1
-        except IndexError:
-            print("IndexError: list index out of range")
-            break
+            print("{:d}".format(my_list[i]), end='')
+            count += 1
+        except (TypeError, ValueError):
+            continue
     print()
     return count
