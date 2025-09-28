79fac20a1557bcaffb53383271ec95037c763b1a
Priam <priamdmy@gmail.com>
2025-09-28 11:35:57 +0200

    [fix] and type(obj) != a_class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -15,4 +15,7 @@ def inherits_from(obj, a_class):
         obj (any): variable to check the class of
         a_class: class we want to check in relation to obj's class
     """
-    return isinstance(type(obj), a_class)
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
