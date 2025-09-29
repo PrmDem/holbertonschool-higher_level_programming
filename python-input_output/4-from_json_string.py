833fbf201bce5f651c9751f87b4da80fc04da408
Priam <priamdmy@gmail.com>
2025-09-29 17:05:13 +0200

    [first] pycodestyle & output ok

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import json
+
+""" Uses the `json.loads()` method to deserialize
+    a Python object from a JSON-formatted string.
+"""
+
+
+def from_json_string(my_str):
+    """Returns the Python representation of an object (string).
+
+    Args:
+        my_obj (str): The JSON string.
+
+    Returns:
+        str: The Python string representation of the object.
+    """
+    return json.loads(my_str)
