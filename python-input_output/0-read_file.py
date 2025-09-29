ae271a50a1a24c62991bddafc030fe999a14826f
Priam <priamdmy@gmail.com>
2025-09-29 11:26:05 +0200

    [first] pycodestyle & output ok

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Prints the contents of a file
+    line by line
+"""
+
+
+def read_file(filename=""):
+    """parses through a file and prints it
+
+    Args:
+        filename: name of the file to open
+
+    Returns nothing."""
+
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+    print(read_data, end='')
