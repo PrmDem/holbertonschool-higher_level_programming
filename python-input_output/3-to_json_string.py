bf62d5de1e56d3fdc2b3848235bda0edc7286d25
Priam <priamdmy@gmail.com>
2025-09-29 16:42:49 +0200

    [first] pycodestyle & output ok

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import json
+
+"""Uses the `json.dumps()` method
+    to serialize a Python object
+    to a JSON-formatted string.
+"""
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj (any): The Python object to serialize.
+
+    Returns:
+        str: The JSON string representation of the object.
+    """
+    return json.dumps(my_obj)
