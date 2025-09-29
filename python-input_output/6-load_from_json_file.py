2477d1f568b7c08b5f8bc34e7a6f21e76fa3532c
Priam <priamdmy@gmail.com>
2025-09-29 19:32:10 +0200

    [first] pycodestyle & output test ok

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import json
+
+"""Creates an Object from a JSON file."""
+
+
+def load_from_json_file(filename):
+    """Load data from a JSON file.
+
+    Opens the specified JSON file, reads its contents using UTF-8-SIG encoding,
+    and returns the deserialized Python object.
+
+    Args:
+        filename (str): The path to the JSON file to be loaded.
+
+    Returns:
+        Any: The Python object resulting from the JSON deserialization.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+        json.JSONDecodeError: If the file does not contain valid JSON.
+    """
+    with open(filename, 'r', encoding="utf-8-sig") as f:
+        data = json.load(f)
+    return data
