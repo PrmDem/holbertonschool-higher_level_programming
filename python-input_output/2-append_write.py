6a25662703b727724cd8b2fd46a91dba9fb079f9
Priam <priamdmy@gmail.com>
2025-09-29 16:30:47 +0200

    [first] pycodestyle & output ok

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def append_write(filename="", text=""):
+    """Appends a string to the end of a text file (UTF-8)
+            and returns the number of characters added.
+
+    Opens the file in append mode and writes the specified text to the end.
+    If the file does not exist, it will be created.
+
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+
+    Returns:
+        int: The number of characters written.
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
