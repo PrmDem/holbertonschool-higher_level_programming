3831bacfa8adcfe8cb194db4cbe5bf94e8ffc54a
Priam <priamdmy@gmail.com>
2025-10-01 13:09:22 +0200

    [fix] added module docstring

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+"""This module, written in Python, uses I/O
+    to append a string of text to a file.
+"""
+
+
 def append_write(filename="", text=""):
     """Appends a string to the end of a text file (UTF-8)
             and returns the number of characters added.
