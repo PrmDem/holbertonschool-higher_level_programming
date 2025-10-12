844c24b6f5d7d8794640e7414854d850e4170127
Priam <priamdmy@gmail.com>
2025-10-12 18:28:46 +0200

    [fix] pycodestyle ok

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,14 +9,14 @@ Parameters:
     text (str): The text to be processed and printed.
 
 Known issues:
-    Because lstrip() does not work,
+    Because lstrip() does not seem to work,
     Replacing 'char + " "' means that an ending period
     with no whitespace after it, will not be replaced by new lines.
 """
 
 
 def text_indentation(text):
-    """breaks provided text 
+    """breaks provided text
     by adding new lines at specific signs
 
     Args:
@@ -30,7 +30,8 @@ def text_indentation(text):
         raise TypeError("text must be a string")
 
     for char in ".?:":
-        text = text.replace(char + " ", char + "\n\n")
-        # removes white space, inserts two new lines
+        if char + " ":
+            text = text.replace(char + " ", char + "\n\n")
+            # removes white space, inserts two new lines
 
     print(text)
