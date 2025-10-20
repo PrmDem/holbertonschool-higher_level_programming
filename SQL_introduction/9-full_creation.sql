7b3da05c5432831407a4aca87744601e8ee88ee1
Priam <priamdmy@gmail.com>
2025-10-20 19:10:15 +0200

    [first] output & comments ok

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,18 @@
+-- creates a new table in db hbtn_0c_0
+-- adds four rows with id, name, and score values
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT);
+
+INSERT INTO second_table
+VALUES (1, 'John', 10);
+
+INSERT INTO second_table
+VALUES (2, 'Alex', 3);
+
+INSERT INTO second_table
+VALUES (3, 'Bob', 14);
+
+INSERT INTO second_table
+VALUES (4, 'George', 8);
