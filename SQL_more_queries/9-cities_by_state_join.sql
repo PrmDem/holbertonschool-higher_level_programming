8c81176bf646ec6e0711c814c254a5724f2a8e44
Priam <priamdmy@gmail.com>
2025-10-21 20:35:41 +0200

    [first] comment & output ok

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- lists cities in db in ascending id order
+-- columns: cities.id, cities.name, states.name
+
+SELECT cities.id, cities.name, states.name
+FROM cities LEFT JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
