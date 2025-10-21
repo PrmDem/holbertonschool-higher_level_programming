e1e035dafafd66a0f2b3eb130fa802559e1509c5
Priam <priamdmy@gmail.com>
2025-10-21 22:45:06 +0200

    [first]comment & output ok

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- lists all show titles + genre id
+-- if there is no genre, displays 'null'
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+GROUP BY tv_shows.title, tv_show_genres.genre_id
+ORDER BY tv_shows.title ASC;
