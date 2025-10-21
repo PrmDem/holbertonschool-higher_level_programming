95e2017509f0b3fcf259013b043be44b106b058c
Priam <priamdmy@gmail.com>
2025-10-21 14:04:22 +0200

    [fix] removed uppercase in password, was needed for tests only

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- create user_0d_1 with all privileges
 -- script should not fail if user already exists
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'User_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
