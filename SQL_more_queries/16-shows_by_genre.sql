ce574011739c06b6962616ac002a00f984bdf396
Priam <priamdmy@gmail.com>
2025-10-22 13:26:09 +0200

    [first] comments & output ok

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- lists all shows by title as many times as they have genres
+-- if no genre, lists once, second column displays NULL
+
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+    JOIN tv_show_genres 
+        ON tv_shows.id = tv_show_genres.show_id
+    JOIN tv_genres
+        ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_shows.title, tv_genres.name
+ORDER BY tv_shows.title ASC;
