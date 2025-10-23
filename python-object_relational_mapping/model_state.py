75f2d0af36fb8cb0f8e3611c4ecb4570e2027402
Priam <priamdmy@gmail.com>
2025-10-23 16:57:11 +0200

    [fix] added code found in 6-model_state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,19 +4,28 @@
 and links to 'states' MySQL table
 """
 
+import sys
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Defines class State
+
+    Args:
+        id (int): unique identifier for a State instance
+        name (str): name of the State instance
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+
 if __name__ == "__main__":
-    from sqlalchemy import Column, Integer, String
-    from sqlalchemy.ext.declarative import declarative_base
-
-    Base = declarative_base()
-
-    class State(Base):
-        """Defines class State
-
-        Args:
-            id (int): unique identifier for a State instance
-            name (str): name of the State instance
-        """
-        __tablename__ = 'states'
-        id = Column(Integer, primary_key=True, nullable=False)
-        name = Column(String(128), nullable=False)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
