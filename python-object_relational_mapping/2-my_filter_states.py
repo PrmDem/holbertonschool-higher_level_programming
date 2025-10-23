d1e7a7784a24ee8161711dac9bad5ed59c2eaa85
Priam <priamdmy@gmail.com>
2025-10-23 13:35:57 +0200

    [first] output tests ok

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""This script lists all states from the database
+that fit the state name passed as a command line argument
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states
+                WHERE name = %s ORDER BY id ASC""", (sys.argv[4],))
+
+    for item in cur.fetchall():
+        print(item)
+
+    cur.close()
+    db.close()
