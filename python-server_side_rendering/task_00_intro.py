76cbedf069562498b52e4394b453363716ccc5ff
Priam <priamdmy@gmail.com>
2025-11-12 14:02:07 +0100

    [fix] added final . to error messages

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -23,11 +23,11 @@ def generate_invitations(template, attendees):
 
     # verifies template is not empty
     if len(template) == 0:
-        print("Template is empty, no output files generated")
+        print("Template is empty, no output files generated.")
 
     # verifies attendees list is not empty
     if len(attendees) == 0:
-        print("No data provided, no output files generated")
+        print("No data provided, no output files generated.")
 
     for (index, inv) in enumerate(attendees):
         invite = template
@@ -35,7 +35,7 @@ def generate_invitations(template, attendees):
 
         # verifies output file doesn't exist
         if os.path.exists(outputFile):
-            print("This file already exists, no new output file generated.")
+            print("This file already exists, no output file generated.")
             return
 
         # gets values and injects them in invite text
